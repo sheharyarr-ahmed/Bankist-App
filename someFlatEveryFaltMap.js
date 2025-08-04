3de56251b05dbabe048ad6f333cf7c3ecfa59088
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-08-05 04:07:23 +0500

    got to know about the flat, flatMap methods

--- someFlatEveryFaltMap.js
+++ someFlatEveryFaltMap.js
@@ -1,3 +1,4 @@
+// some, every and flat and flatMap methods
 'use strict';
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -15,3 +16,12 @@ const arr = [5, 6, 7, 8, 99];
 
 const toCheck = arr.every(mov => mov >= 5);
 console.log(toCheck);
+
+const nestedArr = [[200, 450, -400], 3000, -650, [-130, 70], 1300]; //flatens the array.
+
+console.log(nestedArr.flat());
+
+const moreDeepArray = [[200, 450, -400], 3000, -650, [-130, [70]], 1300];
+console.log(moreDeepArray); //without flat's parameter
+
+console.log(moreDeepArray.flat(2)); //by giving paramter the inner level array are also gone.
