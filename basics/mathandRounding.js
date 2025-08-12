7dbafa46464eccedaf21631223b821583659e8b0
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-08-12 19:52:48 +0500

    learned about the math and rounding fucntions

---
+++ basics/mathandRounding.js
@@ -0,0 +1,33 @@
+'use strcit';
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(2, 3, 4, 8));
+console.log(Math.max(2, 3, '4', '8')); // it does type coercion
+console.log(Math.min(1, 2, 3, 4));
+
+console.log(Math.PI * Number.parseFloat('2.5px') ** 2);
+
+// generating random numbers
+
+console.log(Math.trunc(Math.random() * 6 + 1));
+
+// fucntion created of generating number with in the given range
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+randomInt(10, 20);
+
+// now rounding the integers
+console.log(Math.round(23.3));
+console.log(Math.round(23.8));
+
+console.log(Math.ceil(23.4)); //it will upscale it
+console.log(Math.floor(32.9));
+console.log(Math.trunc(32.55678666)); // trunc removes the all decimals
+
+// rounding the decimals
+console.log((2.7).toFixed(0));
+console.log((3.222).toFixed(2)); //the no tells how many decimals we would like to show after the decimal point. and it returns the string
+console.log(+(3.222).toFixed(2));
--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>`;
+        <div class="movements__value">${mov.toFixed(2)}€</div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
@@ -98,7 +98,7 @@ const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce(function (acc, mov) {
     return acc + mov;
   }, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 // adding new feature calculate display summary, chaining of map, filrer and reduce involved.
@@ -106,19 +106,19 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => int >= 1)
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 // in this new function we learned about the useage of map
@@ -230,7 +230,7 @@ btnTransfer.addEventListener('click', function (e) {
 //implementing the loan function
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // add the loan's movement
