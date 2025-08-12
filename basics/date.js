f690aad4d05ffccc2985c415eae040d0d94700f7
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-08-12 21:38:26 +0500

    got to know about the date method

---
+++ basics/date.js
@@ -0,0 +1,12 @@
+'use strcit';
+console.log(new Date());
+console.log(new Date(2019, 11, 23, 1, 56, 23)); // myear, month, date, hour, min, sec
+const date = new Date();
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.toISOString());
+console.log(date.getSeconds());
+
+console.log(Date.now());
