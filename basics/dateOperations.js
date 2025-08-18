5b3c29e66b306eb4ebfdc87e171f179a1c49329b
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-08-19 04:15:43 +0500

    got to know about the numberFormat in the internationalization

--- basics/dateOperations.js
+++ basics/dateOperations.js
@@ -13,3 +13,24 @@ console.log(+future);
 
 const calcDaysPassed = (date1, date2) =>
   (date2 - date1) / (1000 * 60 * 60 * 24);
+
+//  adding commas according to the locale string/ country and the new method of internalization
+const num = 288888888888.87;
+const options = {
+  style: 'currency',
+  // unit: 'celcius',
+  // style: 'percentage'
+  // style: 'unit'
+  currency: 'EUR',
+};
+
+console.log('US       ', new Intl.NumberFormat('en-US', options).format(num));
+console.log(
+  'Germany       ',
+  new Intl.NumberFormat('de-DE', options).format(num)
+);
+console.log(
+  'Syria       ',
+  new Intl.NumberFormat('ar-SY', options).format(num)
+);
+console.log(navigator.language, new Intl.NumberFormat('en-US').format(num));
--- script.js
+++ script.js
@@ -96,6 +96,15 @@ const formatMovementDate = function (date, locale) {
   // }
   return new Intl.DateTimeFormat(locale).format(date);
 };
+
+// making an new function in order to prevent of repeating of it
+const formattedCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
   const combinedMovsDates = acc.movements.map((mov, i) => ({
@@ -113,12 +122,14 @@ const displayMovements = function (acc, sort = false) {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(movementDate);
     const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formattedCur(movement, acc.locale, acc.currency);
     const html = `<div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
       <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${movement.toFixed(2)}€</div>`;
+        <div class="movements__value">${formattedMov}</div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
@@ -129,7 +140,12 @@ const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce(function (acc, mov) {
     return acc + mov;
   }, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+  // const formattedMov = formattedCur(acc.balance, acc.locale, acc.currency);
+  labelBalance.textContent = formattedCur(
+    acc.balance,
+    acc.locale,
+    acc.currency
+  );
 };
 
 // adding new feature calculate display summary, chaining of map, filrer and reduce involved.
@@ -137,19 +153,27 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formattedCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
+  labelSumOut.textContent = formattedCur(
+    Math.abs(out),
+    acc.locale,
+    acc.currency
+  );
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => int >= 1)
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = formattedCur(
+    interest,
+    acc.locale,
+    acc.currency
+  );
 };
 
 // in this new function we learned about the useage of map
